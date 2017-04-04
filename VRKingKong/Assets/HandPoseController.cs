e2345b00d499c0c15d3b9bc9de3fe7632eff0633
DESKTOP-TCL675U\alexf <alekseif@gmail.com>
2017-04-03 23:10:24 -0400

    Gorilla Hands, with controls for hand poses

--- VRKingKong/Assets/HandPoseController.cs
+++ VRKingKong/Assets/HandPoseController.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using VRTK;
 
 public class HandPoseController : MonoBehaviour {
 
-    public GameObject left;
-    public GameObject right;
-    // Use this for initialization
-
-    Animator animl;
-    Animator animr;
+    public GameObject hand;
+    Animator anim;
 
     int Idle = Animator.StringToHash("Idle");
     int Point = Animator.StringToHash("Point");
@@ -31,21 +27,63 @@ public class HandPoseController : MonoBehaviour {
     int Rock = Animator.StringToHash("Rock");
     int Natural = Animator.StringToHash("Natural");
 
+    bool gripPressed = false;
+    bool triggerPressed = false;
     void Start () {
-        animl = left.GetComponent<Animator>();
-        animr = right.GetComponent<Animator>();
+        anim = hand.GetComponent<Animator>();
+        anim.CrossFade(GrabLarge, 0.6f);
+
+        if (GetComponent<VRTK_ControllerEvents>() == null)
+        {
+            Debug.LogError("VRTK_ControllerEvents_ListenerExample is required to be attached to a Controller that has the VRTK_ControllerEvents script attached to it");
+            return;
+        }
+
+        GetComponent<VRTK_ControllerEvents>().GripPressed += new ControllerInteractionEventHandler(DoGripPressed);
+        GetComponent<VRTK_ControllerEvents>().GripReleased += new ControllerInteractionEventHandler(DoGripReleased);
+
+        GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(DoTriggerPressed);
+        GetComponent<VRTK_ControllerEvents>().TriggerReleased += new ControllerInteractionEventHandler(DoTriggerReleased);
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (OVRInput.GetDown(OVRInput.RawButton.LHandTrigger))
-        {
-            animl.CrossFade(Fist, 0.6f);
-            Debug.Log("prrrressed!");
-        }
-        if (OVRInput.GetUp(OVRInput.RawButton.LHandTrigger))
-        {
-            animl.CrossFade(Natural, 0.6f);
-        }
+
+    // Update is called once per frame
+    private void DebugLogger(uint index, string button, string action, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("Controller on index '" + index + "' " + button + " has been " + action
+                + " with a pressure of " + e.buttonPressure + " / trackpad axis at: " + e.touchpadAxis + " (" + e.touchpadAngle + " degrees)");
+    }
+
+    private void DoGripPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        gripPressed = true;
+        DebugLogger(e.controllerIndex, "GRIP", "pressed", e);
+
+        if(triggerPressed) anim.CrossFade(Fist, 0.6f);
+        else anim.CrossFade(GrabStickFront, 0.6f);
+    }
+
+    private void DoGripReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        gripPressed = false;
+        if (triggerPressed) anim.CrossFade(GrabStickUp, 0.6f);
+        else anim.CrossFade(GrabLarge, 0.6f);
+
+        DebugLogger(e.controllerIndex, "GRIP", "released", e);
+    }
+
+    private void DoTriggerPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        triggerPressed = true;
+        if (gripPressed) anim.CrossFade(Fist, 0.6f);
+        else anim.CrossFade(GrabStickUp, 0.6f);
+        DebugLogger(e.controllerIndex, "TRIGGER", "pressed", e);
+    }
+
+    private void DoTriggerReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        triggerPressed = false;
+        if (gripPressed) anim.CrossFade(GrabStickFront, 0.6f);
+        else anim.CrossFade(GrabLarge, 0.6f);
+        DebugLogger(e.controllerIndex, "TRIGGER", "released", e);
     }
 }
