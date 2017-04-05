7858e988bdb6597d7d270d9fa55c111c9cc5bf74
DESKTOP-LV9ALK1\alexf <alekseif@gmail.com>
2017-04-04 21:03:55 -0400

    Added City

---
+++ VRKingKong/Assets/VRPlayerControllers/Scripts/RayCastHitDetection.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastHitDetection : MonoBehaviour {
+
+	public MoveToAPoint moveToAPoint ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+		float distance = 10.0f;
+		bool didHit = false;
+
+		RaycastHit hit;
+	
+		if (Physics.Raycast(transform.position, fwd * distance , out hit)){
+		
+			//print("Found an object ("+hit.collider.gameObject.name+")- distance: " + hit.distance);
+
+			distance = hit.distance;
+			didHit = true;
+
+			// Load prefab from resource folder
+			if(Input.GetButtonDown("Fire1")){
+
+				GameObject marker = Instantiate(Resources.Load("Marker"), hit.point, Quaternion.identity) as GameObject;
+				Destroy(marker, 4.0f);
+
+
+				if(moveToAPoint){
+					moveToAPoint.targetPoint = hit.point;
+				}
+
+			}
+		}
+			
+		// Draw a line 
+		Debug.DrawRay(transform.position , fwd * distance , didHit ? Color.red : Color.green , 0.1f );
+
+	}
+}
---
+++ VRKingKong/Assets/VRPlayerControllers/Scripts/VRWalker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRWalker : MonoBehaviour {
+
+	CharacterController controller;
+	float currentSpeed = 5.0F;
+
+	public GameObject cam;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ForwardMovementOnKeys();
+	}
+		
+	void ForwardMovementOnKeys()
+	{
+		Vector3 forward = cam.transform.TransformDirection(Vector3.forward);
+		Vector3 right = cam.transform.TransformDirection(Vector3.right);
+
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 horizontalVec3 = right * horizontal * currentSpeed;
+		Vector3 verticalVec3 = forward * vertical * currentSpeed;
+
+		controller.SimpleMove( horizontalVec3 + verticalVec3);
+	}
+}
